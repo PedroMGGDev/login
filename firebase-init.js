71d6649d93a7ba6defd37daa3ff7f96d4c2bbf78
025ambrosio <ambrosiolucas025@gmail.com>
2024-12-06 01:03:22 -0300

    commit

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBAQ6TVoiHDNE58bz-hmYabP6FD6_iqEdQ",
+  authDomain: "controle-de-gastos-3703a.firebaseapp.com",
+  projectId: "controle-de-gastos-3703a",
+  storageBucket: "controle-de-gastos-3703a.appspot.com",
+  messagingSenderId: "697815545565",
+  appId: "1:697815545565:web:d38bc8df64b95b65234d34"
+};
+
+firebase.initializeApp(firebaseConfig);
